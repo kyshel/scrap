09f39de905bd246513d1c1519976abf5466e18ab
kyshel <1069072177@qq.com>
2018-09-01 17:22:50 +0800

    Update merge_questions_options.js

--- merge_questions_options.js
+++ merge_questions_options.js
@@ -1,45 +1,30 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript">
-      console.log('111')
-
-      //console.log($('.qt_content pre')[1])
-      
-       // console.log($($('pre')[0]).text())
+      console.log('this is merge_questions_options.js')
+      // console.log($('.qt_content pre')[1])
+      // console.log($($('pre')[0]).text())
       // console.log($('.roundedText'))
 
-      console.log($('img'))
-
       total_num = $('pre').length
-
-      var ask = 0;
-      option = 0
+      index_question = 0;
+      index_option = 0
 
       container = ''
-
       for (var i = total_num - 1; i >= 0; i--) {
-
-
         entry = ''
+        entry += $($('pre')[index_question++]).text()
 
-        entry += $($('pre')[ask++]).text()
-
-        entry += $($('.roundedText')[option++]).text() + '\n'
-        entry += $($('.roundedText')[option++]).text() + '\n'
-        entry += $($('.roundedText')[option++]).text() + '\n'
-        entry += $($('.roundedText')[option++]).text() + '\n'
+        entry += $($('.roundedText')[index_option++]).text() + '\n'
+        entry += $($('.roundedText')[index_option++]).text() + '\n'
+        entry += $($('.roundedText')[index_option++]).text() + '\n'
+        entry += $($('.roundedText')[index_option++]).text() + '\n'
 
         container += entry
 
-
-        // console.log(ask)
-        // console.log(option)
-        
+        // console.log(index_question)
+        // console.log(index_option)
       }
 
-
-      //console.log(container)
-
-
-
+      console.log(container)
       
     </script>
